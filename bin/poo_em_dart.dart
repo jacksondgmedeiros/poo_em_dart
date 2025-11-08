1c3b9416e394375029ac90b055e565ea569abd5a
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-11-07 22:55:03 -0300

    criação do projeto em dart para aprender os conceitos de POO, projeto iniciado do zero

---
+++ bin/poo_em_dart.dart
@@ -0,0 +1,5 @@
+import 'package:poo_em_dart/poo_em_dart.dart' as poo_em_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${poo_em_dart.calculate()}!');
+}
---
+++ test/poo_em_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:poo_em_dart/poo_em_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
